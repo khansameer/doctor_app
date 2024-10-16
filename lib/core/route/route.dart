khansameer/doctor_app_clone

added patient info

lib/core/route/route.dart
@@ -12,5 +12,5 @@ class RouteName {
   static const String healthFeedScreen = '/HealthFeedScreen';
   static const String profileScreen = '/ProfileScreen';
   static const String patientsScreen = '/PatientsScreen';
-
+  static const String patientInfoScreen = '/PatientInformationScreen';
 }

lib/core/route/route_generator.dart
@@ -7,6 +7,7 @@ import 'package:doctor_app/screen/authentication/login/view/signup_view.dart';
 import 'package:doctor_app/screen/dashboard/dashboard_screen.dart';
 import 'package:doctor_app/screen/dashboard/health_feed/health_feed_screen.dart';
 import 'package:doctor_app/screen/dashboard/notification/notificatio_screen.dart';
+import 'package:doctor_app/screen/dashboard/paitent_screen/patient_information_screen.dart';
 import 'package:doctor_app/screen/dashboard/paitent_screen/patients_screen.dart';
 import 'package:doctor_app/screen/dashboard/profile/profile_screen.dart';
 import 'package:doctor_app/screen/dashboard/setting/setting_screen.dart';
@@ -14,7 +15,6 @@ import 'package:doctor_app/screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../../screen/dashboard/report_and_issue/report_and_issue_screen.dart';
-
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -51,43 +51,40 @@ class RouteGenerator {
       case RouteName.dashboardScreen:
         return MaterialPageRoute(
             builder: (_) => const DashboardScreen(),
-            settings:
-            const RouteSettings(name: RouteName.dashboardScreen));
+            settings: const RouteSettings(name: RouteName.dashboardScreen));
       case RouteName.notificationScreen:
         return MaterialPageRoute(
             builder: (_) => const NotificationScreen(),
-            settings:
-            const RouteSettings(name: RouteName.notificationScreen));
+            settings: const RouteSettings(name: RouteName.notificationScreen));
       case RouteName.settingScreen:
         return MaterialPageRoute(
             builder: (_) => const SettingScreen(),
-            settings:
-            const RouteSettings(name: RouteName.settingScreen));
-
-
+            settings: const RouteSettings(name: RouteName.settingScreen));
       case RouteName.reportAndIssueScreen:
         return MaterialPageRoute(
             builder: (_) => const ReportAndIssueScreen(),
             settings:
-            const RouteSettings(name: RouteName.reportAndIssueScreen));
+                const RouteSettings(name: RouteName.reportAndIssueScreen));
 
       case RouteName.healthFeedScreen:
         return MaterialPageRoute(
             builder: (_) => const HealthFeedScreen(),
-            settings:
-            const RouteSettings(name: RouteName.healthFeedScreen));
+            settings: const RouteSettings(name: RouteName.healthFeedScreen));
 
       case RouteName.profileScreen:
         return MaterialPageRoute(
             builder: (_) => const ProfileScreen(),
-            settings:
-            const RouteSettings(name: RouteName.profileScreen));
+            settings: const RouteSettings(name: RouteName.profileScreen));
 
       case RouteName.patientsScreen:
         return MaterialPageRoute(
             builder: (_) => const PatientsScreen(),
-            settings:
-            const RouteSettings(name: RouteName.patientsScreen));
+            settings: const RouteSettings(name: RouteName.patientsScreen));
+      case RouteName.patientInfoScreen:
+        return MaterialPageRoute(
+          builder: (ctx) => const PatientInformationScreen(),
+          settings: const RouteSettings(name: RouteName.patientInfoScreen),
+        );
 
       default:
         return MaterialPageRoute(

khansameer/doctor_app

lib/core/route/route.dart
@@ -23,5 +23,5 @@ class RouteName {
   static const String patientStoriesScreen = '/PatientStoriesScreen';
   static const String reachScreen = '/ReachScreen';
   static const String weeklyEarningScreen = '/WeeklyEarningScreen';
-
+  static const String appoimentDetail = '/AppoimentDetail';
 }

lib/core/route/route_generator.dart
@@ -4,6 +4,7 @@ import 'package:doctor_app/screen/authentication/forgot/forgot_password_reset_vi
 import 'package:doctor_app/screen/authentication/forgot/forgot_screen.dart';
 import 'package:doctor_app/screen/authentication/login/login_screen.dart';
 import 'package:doctor_app/screen/authentication/login/view/signup_view.dart';
+import 'package:doctor_app/screen/dashboard/appoiment/appoiment_detail.dart';
 import 'package:doctor_app/screen/dashboard/calender/calender_screen.dart';
 import 'package:doctor_app/screen/dashboard/chat_screen/chat_screen.dart';
 import 'package:doctor_app/screen/dashboard/consult/consult_screen.dart';
@@ -113,7 +114,6 @@ class RouteGenerator {
       case RouteName.reportScreen:
         return MaterialPageRoute(
             builder: (_) => const ReportScreen(),
-
             settings: const RouteSettings(name: RouteName.reportScreen));
       case RouteName.editprofileScreen:
         return MaterialPageRoute(
@@ -129,26 +129,28 @@ class RouteGenerator {
       case RouteName.primeScreen:
         return MaterialPageRoute(
             builder: (_) => const PrimeScreen(),
-            settings:
-            const RouteSettings(name: RouteName.primeScreen));
+            settings: const RouteSettings(name: RouteName.primeScreen));
 
       case RouteName.patientStoriesScreen:
         return MaterialPageRoute(
             builder: (_) => const PatientStoriesScreen(),
             settings:
-            const RouteSettings(name: RouteName.patientStoriesScreen));
+                const RouteSettings(name: RouteName.patientStoriesScreen));
 
       case RouteName.reachScreen:
         return MaterialPageRoute(
             builder: (_) => const ReachScreen(),
-            settings:
-            const RouteSettings(name: RouteName.reachScreen));
+            settings: const RouteSettings(name: RouteName.reachScreen));
 
       case RouteName.weeklyEarningScreen:
         return MaterialPageRoute(
             builder: (_) => const WeeklyEarningScreen(),
-            settings:
-            const RouteSettings(name: RouteName.weeklyEarningScreen));
+            settings: const RouteSettings(name: RouteName.weeklyEarningScreen));
+      case RouteName.appoimentDetail:
+        return MaterialPageRoute(
+          builder: (ctx) => const AppoimentDetail(),
+          settings: const RouteSettings(name: RouteName.appoimentDetail),
+        );
       default:
         return MaterialPageRoute(
             builder: (_) =>